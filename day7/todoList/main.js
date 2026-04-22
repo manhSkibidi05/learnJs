e9a485193cc5ce82c5a743d63bc4c8a3f2ba6bac
Phan Van Manh <manhphanvp112@gmail.com>
2026-04-23 05:00:30 +0700

    update morning 5pm

--- day7/todoList/main.js
+++ day7/todoList/main.js
@@ -1,6 +1,6 @@
 let tasks = [];
 let tasksDone = [];
-let taskWait = [];
+let tasksWait = [];
 
 // chức năng thêm mới task
 const addInput = document.querySelector(`.add-input`);
@@ -8,41 +8,101 @@ const addButton = document.querySelector(`.add-button`);
 
 function addNewTask(task){
     tasks.push(task);
+    tasksWait.push(task);
+    showAllTask(task);
+    filterAll(tasks);
+    filterWait(tasksWait);
 }   
 
 // chức năng hiển thị và cập nhật trạng thái task  
 const show = document.querySelector(`.show`);
 
-function showAllTask(){
-    show.innerHTML = ``;
-    for(task of tasks){
-        show.innerHTML += `<div class="show-task flex justify-between border-b border-gray-400 items-center mb-3">
-                <div class="flex gap-2">
-                    <input class="show-task-remote" type="checkbox" name="" id="" value="${task}">
-                    <p class="show-task-text">${task}</p>
-                </div>
-                <button type="button" class="show-task-remove bg-red-200 px-3 py-1 rounded-md mb-1 cursor-pointer">Xóa</button>
-            </div>`
-    }
+function showAllTask(task){
+    // tạo cấu trúc hiện thị 1 task
+    const taskDiv = document.createElement(`div`);
+    taskDiv.className = 'show-task flex justify-between border-b border-gray-400 items-center mb-3';
+
+    const taskDivChild = document.createElement(`div`);
+    taskDivChild.className = `flex gap-2`;
+
+    const checkBox = document.createElement(`input`);
+    checkBox.className = `show-task-remote`;
+    checkBox.type = `checkbox`;
+
+    const para = document.createElement(`p`);
+    para.className = `show-task-text`
+    para.textContent = task;
+
+    taskDivChild.appendChild(checkBox);
+    taskDivChild.appendChild(para);
+
+    const buttonRemove = document.createElement(`button`);
+    buttonRemove.className = `show-task-remove bg-red-200 px-3 py-1 rounded-md mb-1 cursor-pointer`;
+    buttonRemove.textContent = `Xóa`;
+    buttonRemove.type = `button`;
+
+    taskDiv.appendChild(taskDivChild);
+    taskDiv.appendChild(buttonRemove);
+
+    // thêm sự kiện cho task
+    checkBox.addEventListener(`change` , ()=>{
+        if(checkBox.checked){
+            tasksDone.push(task);
+            tasksWait.pop(task);
+            para.classList.add(`line-through`);
+        } else{
+            tasksWait.push(task);
+            tasksDone.pop(task);
+            para.classList.remove(`line-through`);
+        }
+        filterDone(tasksDone);
+        filterWait(tasksWait);
+    })
+
+
+
+
+
+    show.appendChild(taskDiv);
 }
 
 addButton.addEventListener(`click` , ()=>{
     let task = addInput.value
     addNewTask(task);
     addInput.value = ``;
-    showAllTask();
+    
 });
 
-const showTask = document.querySelectorAll(`.show-task`);
-const remoteTask = document.querySelectorAll(`.show-task-remote`);
-const textTask = document.querySelectorAll(`.show-task-text`);
-const removeTask = document.querySelectorAll(`show-task-remove`);
+// chức năng tổng hợp task
+let totalList = document.querySelector(`.total-list`);
+let totalListDone = document.querySelector(`.total-list-done`);
+let totalListWait = document.querySelector(`.total-list-wait`);
 
-remoteTask.forEach((task)=>{
-    task.addEventListener(`change`, ()=>{
-        console.log(task)
-    })
-})
+function filterAll(tasks){
+    let count = 0;
+    for(let task of tasks){
+        count++;
+    }
+    totalList.textContent = `Tổng : ${count}`;
+}
+
+function filterDone(tasks){
+    let count = 0;
+    for(let task of tasks){
+        count++;
+    }
+    totalListDone.textContent = `Hoàn thành : ${count}`;
+}
+
+function filterWait(tasks){
+    let count = 0;
+    for(let task of tasks){
+        count++;
+    }
+    totalListWait.textContent = `Chưa làm : ${count}`;
+}
+
+
 
 
 
