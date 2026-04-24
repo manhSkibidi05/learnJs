13918ef3290745370b0613806192f7627bfbed77
Phan Van Manh <manhphanvp112@gmail.com>
2026-04-24 18:31:41 +0700

    completed todolist by myself

--- day7/todoList/main.js
+++ day7/todoList/main.js
@@ -59,23 +59,27 @@ function createNewTask(task){
         }
         totalTasks(tasks);
 
-        taskDiv.classList.add(`hidden`);
+        taskDiv.remove();
     })
 
     show.appendChild(taskDiv);
 }
 
 addButton.addEventListener(`click` , ()=>{
-    let task = {
+    if(addInput.value){
+        let task = {
         content : addInput.value,
         completed : false
-    };
-    addInput.value = ``;
-    
-    addNewTask(task);
-    createNewTask(task);
+        };
+        
+        addNewTask(task);
+        createNewTask(task);
 
-    totalTasks(tasks);
+        totalTasks(tasks);
+    }
+    addInput.value = ``;
+
+    
 });
 
 // chức năng tổng hợp task
@@ -105,13 +109,98 @@ function totalTasks(tasks){
 let filterBtnAll = document.querySelector(`.filter-all`);
 let filterBtnDone = document.querySelector(`.filter-done`);
 let filterBtnWait = document.querySelector(`.filter-wait`);
-let tasksCompleted = tasks.filter(value=> value[`completed`] === true);
-let tasksUnCompleted = tasks.filter(value=> value[`completed`] === false);
 
+function updateTask(){
+    let tasksShow = document.querySelectorAll(`.show-task-remote`);
+    tasksShow.forEach(task=>{
+        task.closest(`.show-task`).classList.remove(`hidden`);
+    })
+}
 
+function updateTaskDone(){
+    let tasksShow = document.querySelectorAll(`.show-task-remote`);
+    tasksShow.forEach(task =>{
+        if(!task.checked){
+            task.closest(`.show-task`).classList.add(`hidden`);
+        }else{
+            task.closest(`.show-task`).classList.remove(`hidden`);
+        }
+    })
+}
 
+function updateTaskWait(){
+    let tasksShow = document.querySelectorAll(`.show-task-remote`);
+    tasksShow.forEach(task =>{
+        if(task.checked){
+            task.closest(`.show-task`).classList.add(`hidden`);
+        }else{
+            task.closest(`.show-task`).classList.remove(`hidden`);
+        }
+    })
+}
+filterBtnAll.addEventListener(`click` , ()=>{
+    updateTask();
+    if(!filterBtnAll.classList.contains(`bg-red-200`)){
+        filterBtnAll.classList.add(`bg-red-200`);
+        filterBtnDone.classList.remove(`bg-red-200`);
+        filterBtnWait.classList.remove(`bg-red-200`);
+    }
+});
 
+filterBtnDone.addEventListener(`click` , ()=>{
+    updateTaskDone();
+    if(!filterBtnDone.classList.contains(`bg-red-200`)){
+        filterBtnDone.classList.add(`bg-red-200`);
+        filterBtnAll.classList.remove(`bg-red-200`);
+        filterBtnWait.classList.remove(`bg-red-200`);
+    }
+});
 
+filterBtnWait.addEventListener(`click` , ()=>{
+    updateTaskWait();
+    if(!filterBtnWait.classList.contains(`bg-red-200`)){
+        filterBtnWait.classList.add(`bg-red-200`);
+        filterBtnDone.classList.remove(`bg-red-200`);
+        filterBtnAll.classList.remove(`bg-red-200`);
+    }
+});
 
+// chức năng tìm kiếm 
+const searchInput = document.querySelector(`.search-input`);
+const searchWarning = document.querySelector(`.search-warning`);
 
+function searchTask(string){
+    let textTasks = document.querySelectorAll(`.show-task-text`);
+    if(!string){
+        searchWarning.textContent = `không tồn tại`;
+    }else{
+        searchWarning.textContent = ``;
+    }
+    for(let char of string){
+        for(let task of textTasks){
+            if(task.textContent.indexOf(char) >= 0){
+                task.closest(`.show-task`).classList.remove(`hidden`);
+            }else{
+                task.closest(`.show-task`).classList.add(`hidden`);
+            }
+        }
+    }
+}
+
+function debounce(func , timeout){
+    let idTimeout;
+    return function(...args){
+        clearTimeout(idTimeout);
+        idTimeout = setTimeout(()=>{
+            func.apply(this , args);
+        },timeout)
+    }
+}
+
+const debounceSearch = debounce(searchTask , 500);
+
+searchInput.addEventListener(`input` , ()=>{
+    let valueInput = searchInput.value;
+    debounceSearch(valueInput);
+})
 
