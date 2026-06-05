04f309de04653b42d9952a01c852a126c3fb6105
Phan Van Manh <manhphanvp112@gmail.com>
2026-06-05 17:50:46 +0700

    ôn tập về bất đồng bộ async/await bài 3 và bài 4

---
+++ day14/review.js
@@ -0,0 +1,10 @@
+// review ngày 14 : 
+
+    // - phương thức trong obj : được khởi tạo như 1 thuộc tính trong obj giúp xử lí các dữ
+    // liệu truyền vào từ bên ngoài obj 
+    // -> phương thức trong obj có thể truy cập các thuộc tính thông qua từ khóa this
+
+    // - Từ khóa this trong js : là từ khóa đại diện cho 1 đối tượng được tham chiếu khi 
+    // đối tượng gọi phương thức và trong phương thức đó chứa từ khóa this 
+    // -> hàm callback không có this riêng nó nhận this từ bên ngoài lexical-scope 
+    
